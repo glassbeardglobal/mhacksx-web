c54d21ca1b3102ec7781d9ee45b75aff22278580
Jonathan Xu <jonathanj.xu97@gmail.com>
2017-09-23 11:23:36 -0400

    Story Branching (#3)

--- models/story.js
+++ models/story.js
@@ -23,19 +23,72 @@ exports.get = (id, callback) => {
     });
 };
 
-exports.new = (title, content, callback) => {
+exports.new = (title, content, end, callback) => {
   mongoUtil
     .getDb()
     .collection(collectionName)
     .insertOne({
       title,
       content,
+      end,
       upv: 0,
       dv: 0,
       children: []
     }, (err, result) => {
       callback(err, result);
     });
+};
+
+// takes objectID of original object
+// If ca is null we are branching off the end
+exports.splitUpdate = (origID, title, root, ca, cb, callback) => {
+  const col = mongoUtil.getDb().collection(collectionName);
+
+  exports.new(title, cb, true, (err, res) => {
+    if (err) return callback(err);
+
+    if (ca) {
+      exports.get(origID, (err, origDoc) => {
+        if (err) return callback(err);
+        if (origDoc == null) return callback(new Error("Not Found"));
+
+        col.insertOne({
+          title,
+          content: ca,
+          end: origDoc.children.length == 0,
+          upv: 0,
+          dv: 0,
+          children: origDoc.children
+        }, (err, result) => {
+          if (err) return callback(err);
+
+          col.updateOne({ _id: ObjectID(origID) }, {
+            $set: {
+              content: root,
+              children: [res.insertedId, result.insertedId]
+            }
+          }, (err, updateres) => {
+            if (err) return callback(err);
+            callback(null, {
+              originalChild: res.insertedId,
+              branchedChild: result.insertedId
+            });
+          });
+        });
+      });
+    } else {
+      col.updateOne({ _id: ObjectID(origID) }, {
+        $push: { children: res.insertedId }
+      }, (err, res) => {
+        if (err) return next(err);
+
+        callback(null, {
+          success: true,
+          insertedId: res.insertedId
+        });
+      });
+    }
+  });
 };
 
 exports.delete = (id, callback) => {
